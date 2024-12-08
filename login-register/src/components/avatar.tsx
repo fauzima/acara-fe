573bcad6848c11be92a9f7251e22dbb61c64126a
salmanahadyy <ahady1105@gmail.com>
2024-12-08 10:59:37 +0700

    first commit

---
+++ login-register/src/components/avatar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useSession } from "@/context/useSession";
+import { deleteCookie } from "@/libs/action";
+import { useRouter } from "next/navigation";
+import AvatarMenu from "./avatarMenu";
+
+export const Avatar = () => {
+  const router = useRouter();
+  const { user, isAuth, setIsAuth } = useSession();
+  const onLogout = () => {
+    deleteCookie("token");
+    setIsAuth(false);
+    router.refresh()
+    router.push("/login");
+    router.refresh()
+  };
+
+  return (
+    <>
+      {isAuth ? (
+        <AvatarMenu user={user} onLogout={onLogout} />
+      ) : (
+        <div className="flex gap-2">
+          <button
+            onClick={() => router.push("/organizer/signup")}
+            className="inline-flex items-center border px-3 py-2 text-sm font-medium text-center text-black bg-white rounded-lg hover:bg-gray-100 "
+          >
+            Buat Event
+          </button>
+          <button
+            onClick={() => router.push("/user/login")}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 "
+          >
+            Login
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
